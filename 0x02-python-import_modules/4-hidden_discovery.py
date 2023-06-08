5e2517658bc13e0a19f2eeefe8a74a91171a606b
IanKiguru <iankiguru72@gmail.com>
2023-06-08 12:11:58 -0700

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+import hidden_4
+
+j = 0
 
 if __name__ == '__main__':
- import hidden_4
-  
- for name in sorted(dir(hidden_4)):
-     if not name.startswith('__'):
-         print(name)
+    my_list = dir(hidden_4)
+    new_list = sorted(my_list)
+    while j < len(new_list):
+        if new_list[j][0] != '_':
+            print(new_list[j])
+        j += 1

c2052697f910a8e80901f4d8c9d6f4028b7a5c1d
IanKiguru <iankiguru72@gmail.com>
2023-06-08 11:49:31 -0700

    1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,13 +4,7 @@ if __name__ == "__main__":
 
  a = 10
  b = 5
-
- result_add = add(a, b)
- result_subtract = sub(a, b)
- result_multiply = mul(a, b)
- result_divide = div(a, b)
-
- print("{:d} + {:d} = {:d}".format(a, b, result_add))
- print("{:d} - {:d} = {:d}".format(a, b, result_subtract))
- print("{:d} * {:d} = {:d}".format(a, b, result_multiply))
- print("{:d} / {:d} = {:d}".format(a, b, result_divide))
+ print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
+ print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
+ print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
+ print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))

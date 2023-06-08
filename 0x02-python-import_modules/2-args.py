ad8816c0b5197eeda13ccbc8ad96089b83467bc3
IanKiguru <iankiguru72@gmail.com>
2023-06-08 01:23:14 -0700

    0x02-python-import_modules

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+
+ import sys
+
+ numb_args = len(sys.argv) - 1
+ if numb_args == 0:
+    print("0 arguments.")
+ elif numb_args == 1:
+    print("1 argument:")
+ else:
+    print("{} arguments:".format(numb_args))
+ for j in range(numb_args):
+     print("{}: {}".format(j + 1, sys.argv[j + 1]))
